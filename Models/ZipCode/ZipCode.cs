7e3bf5ea9f3b5608054cefe7926e87802062bc6c
Felipe Santos <felipesantossf@outlook.com>
2024-08-06 20:28:47 -0300

    Add project files.

---
+++ Models/ZipCode/ZipCode.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Models.ZipCode
+{
+	public class ZipCode
+	{
+		[JsonPropertyName("cep")]
+		public string Cep { get; set; } = string.Empty;
+
+		[JsonPropertyName("state")]
+		public string State { get; set; } = string.Empty;
+
+		[JsonPropertyName("city")]
+		public string City { get; set; } = string.Empty;
+
+		[JsonPropertyName("neighborhood")]
+		public string Neighborhood { get; set; } = string.Empty;
+
+		[JsonPropertyName("street")]
+		public string Street { get; set; } = string.Empty;
+
+		[JsonPropertyName("service")]
+		public string Service { get; set; } = string.Empty;
+	}
+}
